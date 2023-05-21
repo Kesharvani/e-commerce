2e3a78d4625d29264acd470742fab575ba2dd16e
Kesharvani <shubhamkesharvani888@gmail.com>
2023-05-21 12:46:03 +0530

    feat: added html and css for filter component

--- src/Components/Filter.jsx
+++ src/Components/Filter.jsx
@@ -6,26 +6,57 @@ const Filter = () => {
         <strong>Filter</strong>
         <button>Clear</button>
       </div>
-      <div className="">
+      <div className="price">
         <strong>Price</strong>
         <label htmlFor="">
           <input type="radio" value="high-to-low" />
           High to Low
         </label>
         <label htmlFor="">
-          {" "}
           <input type="radio" value="low-to-high" />
           Low to high
         </label>
       </div>
-      <div>
-        <strong>Price Range</strong>
+      <div className="price-range">
+        <label for="temp">Price Range</label>
+        <input type="range" id="temp" name="temp" list="markers" />
+        <datalist id="markers">
+          <option value="0"></option>
+          <option value="25"></option>
+          <option value="50"></option>
+          <option value="75"></option>
+          <option value="100"></option>
+        </datalist>
       </div>
-      <div>
+      <div className="categories">
         <strong>Categories</strong>
+        <label htmlFor="">
+          <input type="checkbox" />
+          Books
+        </label>
+        <label htmlFor="">
+          <input type="checkbox" />
+          Clothes
+        </label>
+        <label htmlFor="">
+          <input type="checkbox" />
+          Food
+        </label>
       </div>
-      <div>
+      <div className="rating">
         <strong>Rating</strong>
+        <label htmlFor="">
+          <input type="radio" />1 Star & above
+        </label>
+        <label htmlFor="">
+          <input type="radio" />2 Stars & above
+        </label>
+        <label htmlFor="">
+          <input type="radio" />3 Stars & above
+        </label>
+        <label htmlFor="">
+          <input type="radio" />4 Stars & above
+        </label>
       </div>
     </div>
   );
