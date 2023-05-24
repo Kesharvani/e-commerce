1e51d33e28b1b7bde692664496d81260b68e95b5
Kesharvani <shubhamkesharvani888@gmail.com>
2023-05-24 21:12:41 +0530

    feat:added category services

--- src/Components/Category.jsx
+++ src/Components/Category.jsx
@@ -1,32 +1,40 @@
 import "./Category.css";
 import { Link } from "react-router-dom";
-const foodImage =
-  "https://images.pexels.com/photos/70497/pexels-photo-70497.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1";
-const clothesImage =
-  "https://images.pexels.com/photos/1884584/pexels-photo-1884584.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1";
-const bookImage =
-  "https://images.pexels.com/photos/7171398/pexels-photo-7171398.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1";
+import { useState, useEffect } from "react";
+
+import { categoryService } from "../Services/category/categoryService";
+
 const Category = () => {
+  const [category, setCategory] = useState([]);
+  const getCategoryData = async () => {
+    try {
+      const {
+        data: { categories },
+        status,
+      } = await categoryService();
+      if (status === 200) {
+        setCategory(categories);
+      }
+    } catch (error) {
+      console.error("error in category:", error);
+    }
+  };
+  useEffect(() => {
+    getCategoryData();
+  }, []);
   return (
     <div className="category-container">
-      <section className="category-card">
-        <Link to="/landing">
-          <img src={foodImage} alt="foodImage" />
-        </Link>
-        <strong>Food</strong>
-      </section>
-      <section className="category-card">
-        <Link to="/landing">
-          <img src={clothesImage} alt="foodImage" />
-        </Link>
-        <strong>Clothes</strong>
-      </section>
-      <section className="category-card">
-        <Link to="/landing">
-          <img src={bookImage} alt="foodImage" />
-        </Link>
-        <strong>Books</strong>
-      </section>
+      {category &&
+        category?.map((item) => {
+          return (
+            <section className="category-card" key={item._id}>
+              <Link to="/landing">
+                <img src={item?.imageUrl} alt="foodImage" />
+              </Link>
+              <strong>{item.categoryName.toUpperCase()}</strong>
+            </section>
+          );
+        })}
     </div>
   );
 };
--- src/backend/db/categories.js
+++ src/backend/db/categories.js
@@ -11,23 +11,31 @@ export const categories = [
     categoryName: "book",
     description:
       "A book is a medium for recording information in the form of writing or images, typically composed of many pages.",
+    imageUrl:
+      "https://images.pexels.com/photos/7171398/pexels-photo-7171398.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
   },
   {
     _id: uuid(),
     categoryName: "fashion",
     description:
       "Fashion is a form of self-expression and autonomy at a particular period and place and in a specific context, of clothing, footwear, lifestyle, accessories, makeup, hairstyle, and body posture.",
+    imageUrl:
+      "https://images.pexels.com/photos/1884584/pexels-photo-1884584.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
   },
   {
     _id: uuid(),
     categoryName: "home-appliances",
     description:
       "Household appliance, is a machine which assists in household functions such as cooking, cleaning and food preservation..",
+    imageUrl:
+      "https://media.istockphoto.com/id/1174598609/photo/set-of-contemporary-house-appliances-isolated-on-white.jpg?s=612x612&w=0&k=20&c=bBMILbCpLkhIxbL7sAAXaFOaFaSXFCt80ccHgl7iiWM=",
   },
   {
     _id: uuid(),
     categoryName: "groceries",
     description:
       "Groceries are foods you buy at a grocer's or at a supermarket such as flour, sugar, and tinned foods.",
+    imageUrl:
+      "https://www.gannett-cdn.com/-mm-/a0a28bd666af6d80b33247a358069ae6b7ce0cc4/c=0-108-2121-1306/local/-/media/2016/07/22/USATODAY/USATODAY/636047914661198701-ThinkstockPhotos-467612138.jpg?width=660&height=373&fit=crop&format=pjpg&auto=webp",
   },
 ];
