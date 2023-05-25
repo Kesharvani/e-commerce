430216ce5b2a2dc48f79e3e70d97248a5d6c11d4
Kesharvani <shubhamkesharvani888@gmail.com>
2023-05-25 07:57:47 +0530

    feat: added cart services

---
+++ src/Services/cart/cartServices.js
@@ -0,0 +1,86 @@
+import axios from "axios";
+
+import { ACTION_TYPE } from "../../Utils/Constant";
+export const addToCart = async (product, token, dispatch) => {
+  try {
+    const {
+      data: { cart },
+      status,
+    } = await axios.post(
+      "/api/user/cart",
+      {
+        product,
+      },
+      {
+        headers: {
+          authorization: token,
+        },
+      }
+    );
+    if (status === 200) {
+      dispatch({ type: ACTION_TYPE.ADD_TO_CART, payload: cart });
+    }
+  } catch (error) {
+    console.error("Error in Add to cart service", error);
+  }
+};
+
+export const removeFromCart = async (id, token, dispatch) => {
+  try {
+    const {
+      data: { cart },
+      status,
+    } = await axios.delete(`/api/user/cart/${id}`, {
+      headers: {
+        authorization: token,
+      },
+    });
+    if (status === 200)
+      dispatch({ type: ACTION_TYPE.REMOVE_FROM_CART, payload: cart });
+  } catch (error) {
+    console.error("Error in remove from cart", error);
+  }
+};
+
+export const clearCart = async (cart, token, dispatch) => {
+  try {
+    for (item of cart) {
+      await axios.delete(`/api/user/cart/${item.id}`, {
+        headers: {
+          authorization: token,
+        },
+      });
+    }
+    dispatch({ type: ACTION_TYPE.CLEAR_CART });
+  } catch (error) {
+    console.error("Error in clearing the cart service", error);
+  }
+};
+
+export const updateQuantityFromCart = async (
+  id,
+  token,
+  dispatch,
+  actionType
+) => {
+  try {
+    const {
+      data: { cart },
+      status,
+    } = await axios.post(
+      `/api/user/cart/${id}`,
+      {
+        action: {
+          type: actionType === "INCREASE_QTY" ? "increment" : "decrement",
+        },
+      },
+      {
+        headers: { authorization: token },
+      }
+    );
+    if (status === 200)
+      dispatch({ type: ACTION_TYPE.UPDATE_QTY_CART, payload: cart });
+  } catch (error) {
+    console.error("Error in updating the cart service", error);
+  }
+};
