5ef51e4badcfa1e364c7678b4805874a4fe90c41
Kesharvani <shubhamkesharvani888@gmail.com>
2023-05-28 13:01:43 +0530

    feat: added login field login

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,17 +1,45 @@
-import './LoginRegister.css'
+import "./LoginRegister.css";
 import { Link } from "react-router-dom";
+import { useState } from "react";
+
+import { useAuth } from "../Contexts/AuthContext";
+
 export const Login = () => {
+  const { loginHandler } = useAuth();
+  const [loginData, setLoginData] = useState({
+    email: "",
+    password: "",
+  });
+  const submit = (e) => {
+    e.preventDefault();
+    loginHandler(loginData.email, loginData.password);
+  };
+
   return (
     <div className="login">
       {
-        <form>
+        <form onSubmit={submit}>
           <div className="input-container">
             <label id="label">E-mail Address</label>
-            <input type="text" placeholder="Enter email address" />
+            <input
+              type="text"
+              placeholder="Enter email address"
+              value={loginData.email}
+              onChange={(e) =>
+                setLoginData({ ...loginData, email: e.target.value })
+              }
+            />
           </div>
           <div className="input-container">
             <label id="label">Password</label>
-            <input type="password" placeholder="Enter password" />
+            <input
+              type="password"
+              value={loginData.password}
+              placeholder="Enter password"
+              onChange={(e) =>
+                setLoginData({ ...loginData, password: e.target.value })
+              }
+            />
           </div>
           <div className="signed-forget-container">
             <div className="signed-in">
@@ -19,10 +47,12 @@ export const Login = () => {
               <span>remain signed in</span>
             </div>
             <div>
-              <Link id='link-text'>forget Password</Link>
+              <Link id="link-text">forget Password</Link>
             </div>
           </div>
-          <button className="login-btn">Login</button>
+          <button className="login-btn" onClick={submit}>
+            Login
+          </button>
         </form>
       }
     </div>
--- src/Contexts/AuthContext.jsx
+++ src/Contexts/AuthContext.jsx
@@ -1,11 +1,38 @@
+import { signupServices } from "../Services/signup/signupServices";
+import { loginService } from "../Services/login/loginService";
+
 const { createContext, useContext, useState } = require("react");
 
 export const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
   const [isLoggedin, setIsLoggedIn] = useState(false);
+
+  const signupHandler = async (email, password, firstName, dataOfBirth) => {
+    try {
+      const {
+        data: { user, encodedToken },
+        status,
+      } = await signupServices(email, password, firstName, dataOfBirth);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const loginHandler = async (email, password) => {
+    try {
+      const {
+        data: { foundUser, encodedToken },
+      } = await loginService(email, password);
+      console.log(foundUser, encodedToken, "login details");
+    } catch (error) {
+      console.error(error);
+    }
+  };
   return (
-    <AuthContext.Provider value={{ isLoggedin, setIsLoggedIn }}>
+    <AuthContext.Provider
+      value={{ isLoggedin, setIsLoggedIn, loginHandler, signupHandler }}
+    >
       {children}
     </AuthContext.Provider>
   );
