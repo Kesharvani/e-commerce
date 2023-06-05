4c0c648fffe94a43bd751beb6e668cbabdc0b120
Kesharvani <shubhamkesharvani888@gmail.com>
2023-06-06 04:43:12 +0530

    fix: handled category in home page

--- src/Components/Category.jsx
+++ src/Components/Category.jsx
@@ -1,10 +1,16 @@
 import "./Category.css";
 import { Link } from "react-router-dom";
 import { useState, useEffect } from "react";
-
+import { useProduct } from "../Contexts/ProductContext";
 import { categoryService } from "../Services/category/categoryService";
+import { ACTION_TYPE } from "../Utils";
 
 const Category = () => {
+  const { dispatch } = useProduct();
+  const categoryHandler = (item) => {
+    dispatch({ type: ACTION_TYPE.CLEAR_FILTER });
+    dispatch({ type: ACTION_TYPE.CATEGORY, payload: item.categoryName });
+  };
   const [category, setCategory] = useState([]);
   const getCategoryData = async () => {
     try {
@@ -28,10 +34,10 @@ const Category = () => {
         category?.map((item) => {
           return (
             <section className="category-card" key={item._id}>
-              <Link to="/landing">
+              <Link to="/landing" onClick={() => categoryHandler(item)}>
                 <img src={item?.imageUrl} alt="foodImage" />
               </Link>
-              <strong>{item.categoryName.toUpperCase()}</strong>
+              <strong>{item?.categoryName.toUpperCase()}</strong>
             </section>
           );
         })}
