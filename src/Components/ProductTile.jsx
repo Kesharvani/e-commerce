35184fcb1f78d600a2b69cacbb484b1c9f0df7d2
Kesharvani <shubhamkesharvani888@gmail.com>
2023-06-06 13:16:49 +0530

    fix: added go to cart in product listing page

--- src/Components/ProductTile.jsx
+++ src/Components/ProductTile.jsx
@@ -6,8 +6,9 @@ import {
 } from "../Services/wishlist/wishlistServices";
 import { useAuth } from "../Contexts/AuthContext";
 import { useProduct } from "../Contexts/ProductContext";
-
+import { useNavigate } from "react-router-dom";
 const ProductTile = ({ item, isInCart, isInWishlist }) => {
+  const navigate = useNavigate();
   const { loginToken } = useAuth();
   const {
     dispatch,
@@ -25,22 +26,19 @@ const ProductTile = ({ item, isInCart, isInWishlist }) => {
         <button onClick={() => removeFromCart(item, loginToken, dispatch)}>
           Remove from cart
         </button>
+      ) : cart?.find((data) => data._id === item._id) ? (
+        <button onClick={() => navigate("/cart")}>Go to cart</button>
       ) : (
-        <button
-          onClick={() => addToCart(item, loginToken, dispatch)}
-          disabled={cart?.find((data) => data._id === item._id)}
-        >
+        <button onClick={() => addToCart(item, loginToken, dispatch)}>
           Add to cart
         </button>
       )}
-      {isInWishlist || wishlist?.find((data) => data._id === item._id)? (
+      {isInWishlist || wishlist?.find((data) => data._id === item._id) ? (
         <button onClick={() => removeFromWishlist(item, loginToken, dispatch)}>
           Remove from wishlist
         </button>
       ) : (
-        <button
-          onClick={() => addToWishlist(loginToken, item, dispatch)}
-        >
+        <button onClick={() => addToWishlist(loginToken, item, dispatch)}>
           Add to Wishlist
         </button>
       )}
