aa0960798fac60f78040f4e67b6cde821e1996e6
Kesharvani <shubhamkesharvani888@gmail.com>
2023-08-07 12:09:29 +0530

    style:product tile css

--- src/Components/ProductTile.jsx
+++ src/Components/ProductTile.jsx
@@ -8,6 +8,9 @@ import toast from "react-hot-toast";
 import { useAuth } from "../Contexts/AuthContext";
 import { useProduct } from "../Contexts/ProductContext";
 import { useNavigate, Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHeart } from "@fortawesome/free-solid-svg-icons";
+
 const ProductTile = ({ item, isInCart, isInWishlist, isInIndividualPage }) => {
   const navigate = useNavigate();
   const { loginToken } = useAuth();
@@ -19,37 +22,49 @@ const ProductTile = ({ item, isInCart, isInWishlist, isInIndividualPage }) => {
   return (
     <div className="card">
       <Link to={`/individual/${item._id}`}>
-        <img src={item.thumbnail} alt={`productImage${item.title}`} />
+        <div
+          style={{ backgroundImage: `url(${item.thumbnail})` }}
+          className="image-container"
+        ></div>
+        {/* <img src={item.thumbnail} alt={`productImage${item.title}`} /> */}
       </Link>
       <h3 id="title">{item.title}</h3>
       <p>{item.category}</p>
-      <p>Price:{item.price} QTY:{item.qty}</p>
+      <p>
+        Price:{item.price} QTY:{item.qty}
+      </p>
       {isInIndividualPage && <p>Rating:{item?.rating}</p>}
       {isInCart ? (
         <button
           onClick={() => removeFromCart(item, loginToken, dispatch, toast)}
+          className="btn-cart"
         >
           Remove from cart
         </button>
       ) : cart?.find((data) => data._id === item._id) ? (
-        <button onClick={() => navigate("/cart")}>Go to cart</button>
+        <button onClick={() => navigate("/cart")} className="btn-cart">
+          Go to cart
+        </button>
       ) : (
-        <button onClick={() => addToCart(item, loginToken, dispatch, toast)}>
+        <button
+          onClick={() => addToCart(item, loginToken, dispatch, toast)}
+          className="btn-cart"
+        >
           Add to cart
         </button>
       )}
       {isInWishlist || wishlist?.find((data) => data._id === item._id) ? (
-        <button
+        <FontAwesomeIcon
+          icon={faHeart}
           onClick={() => removeFromWishlist(item, loginToken, dispatch, toast)}
-        >
-          Remove from wishlist
-        </button>
+          className="fill-icon-wishlist"
+        />
       ) : (
-        <button
+        <FontAwesomeIcon
+          icon={faHeart}
           onClick={() => addToWishlist(loginToken, item, dispatch, toast)}
-        >
-          Add to Wishlist
-        </button>
+          className="icon-wishlist"
+        />
       )}
     </div>
   );
